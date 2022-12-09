01d9744b599476a040d7739715ca2f3e3f992998
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-09 21:47:16 +0700

    HWSem3-1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Например: 14212 -> нет 12821 -> да 23432 -> да*/
+
+Console.WriteLine("Enter a five-digit number:");
+string number = Console.ReadLine();
+int length = number.Length;
+
+if (length == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("This number palindrome");
+    }
+    else
+    {
+        Console.WriteLine("This number not palindrome");
+    }
+}
+else
+{
+    Console.WriteLine("Number is not five-digit!");
+}
