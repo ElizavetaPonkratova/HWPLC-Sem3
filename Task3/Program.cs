14d425d6f855b758db22462f88f4161340ef6cd0
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-11 19:24:14 +0700

    HWSem3-3(2.0)

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,14 +1,11 @@
 ﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 Например: 3 -> 1, 8, 27 5 -> 1, 8, 27, 64, 125*/
 
-int ReadInt(string text)
-{
-    Console.Write(text);
-    return Convert.ToInt32(Console.ReadLine());
-}
+Console.WriteLine("Enter number N:");
+int number = int.Parse(Console.ReadLine());
 
-int number = ReadInt("Enter number N:");
 for (int i = 1; i <= number; i++)
 {
     Console.WriteLine(i * i * i);
 }
+
