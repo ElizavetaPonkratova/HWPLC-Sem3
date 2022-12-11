9a0d595b561a6088adee806a5329a06ecd1829f8
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2022-12-11 19:20:41 +0700

    HWSem3-3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Например: 3 -> 1, 8, 27 5 -> 1, 8, 27, 64, 125*/
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = ReadInt("Enter number N:");
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine(i * i * i);
+}
